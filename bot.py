53bcc90982a8dd2b17b7f26d99f11fd493a8edc7
hms5232 <hms5232@yahoo.com.tw>
2020-10-09 13:35:58 +0800

    專案初始設定 + 可以跑的機器人
    之後寫好抓 IP 並丟回給使用者就好

---
+++ bot.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+Author：hms5232
+Repo：https://github.com/hms5232/get-LAN-IP-telegram-bot
+Bug or suggestion：https://github.com/hms5232/get-LAN-IP-telegram-bot/issues
+"""
+
+
+from telegram.ext import Updater, CommandHandler
+import secret
+
+
+# 設定一些個人的環境變數
+telegram_bot_token = secret.token
+
+
+'''
+尚未執行機器人之前，可傳送訊息給機器人後至下列網址查看：
+
+	https://api.telegram.org/bot{$token}/getUpdates
+'''
+updater = Updater(token=telegram_bot_token)  # 呼叫 bot 用
+
+
+def welcome(bot, update):
+	""" Show user welcome message. """
+	
+	chat_id = update.message.from_user.id
+	
+	about_bot = ''
+	about_bot = about_bot + 'Hello! 感謝您的使用\n'
+	about_bot = about_bot + '本機器人由 hms5232 開發\n'
+	about_bot = about_bot + '可於 [Github](https://github.com/hms5232) 或 [Gitlab](https://gitlab.com/hms5232) 上找到我\n'
+	about_bot = about_bot + '聯絡請走[這邊](https://github.com/hms5232/get-LAN-IP-telegram-bot/issues)\n'
+	
+	bot.send_message(chat_id, about_bot, parse_mode='Markdown')
+
+
+def show_user_info(bot, update):
+	""" 回覆使用者的資訊 """
+	
+	user_info = '發送人 id：{}\n'.format(update.message.from_user.id)
+	
+	update.message.reply_text(user_info, disable_notification="True")
+
+
+def get_ip(updater):
+	""" 找 IP 後回給使用者 """
+
+	# TODO: 取得裝置區域網路 ip
+	# 將設定檔中全部的 id 都傳一輪
+	for uid in secret.notify_user_id:
+		updater.bot.send_message(uid, 'Welcome to Telegram & RPi!')
+
+
+updater.dispatcher.add_handler(CommandHandler(['start', 'about'], welcome))  # 歡迎訊息 / 機器人資訊
+updater.dispatcher.add_handler(CommandHandler('info', show_user_info))  # 顯示使用者資訊
+
+
+# 機器人起床!!!
+updater.start_polling()
+# 找 IP
+get_ip(updater)
+# 功成身退
+updater.stop()
+updater.is_idle = False
---
+++ config.py
@@ -0,0 +1,2 @@
+token = 'your_tg_bot_boken'  # 替換字串為你的機器人 token
+notify_user_id = []  # 填入想收到通知的使用者、群組 id
