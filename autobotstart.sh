fcff21553c5c6580f1a2f71b810fbc9a87c07baf
hms5232 <hms5232@yahoo.com.tw>
2020-10-15 21:22:20 +0800

    加上開機時會讀取的 shell script
    設定好後，開機時候會來執行這個 script，就會啟動機器人做事啦

---
+++ autobotstart.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+
+# See more info about this shell script:
+# https://github.com/hms5232/get-LAN-IP-telegram-bot
+
+
+# Change path depend on your env
+cd /home/pi/Documents/get-LAN-IP-telegram-bot
+
+# =======================
+# Please choose one way
+# depend on how you 
+# install python package
+# 
+# and
+# 
+# don't forget edit
+# 	/etc/rc.local
+# run this script
+# when start
+# =======================
+
+# Use Pipenv
+# Change Pipenv binary path depend on your env
+# Change log path if you need (or remove output log command)
+/home/pi/.local/bin/pipenv run python3 bot.py > /home/pi/Documents/get-LAN-IP-telegram-bot/autorunbot.log 2>&1 &
+
+# or
+
+# General (install package directly on system)
+# Change log path if you need (or remove output log command)
+#python3 bot.py > /home/pi/Documents/get-LAN-IP-telegram-bot/autorunbot.log 2>&1 &
